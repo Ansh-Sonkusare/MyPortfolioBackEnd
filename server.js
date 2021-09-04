4e4073b82b7de10f9bd3fefaea69e21a3f96ab14
Ansh-Sonkusare <59761933+Ansh-Sonkusare@users.noreply.github.com>
2021-09-04 19:15:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -74,6 +74,12 @@ transporter.sendMail(mailOptions2, (error, info) => {
 });
 
 })
+
+
+app.get('/', (req, res) => { 
+      res.send('Hello World!')
+  
+  })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 
